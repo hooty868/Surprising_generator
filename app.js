630c967ee302095d28922b4f132e2055b2de5525
hooty868 <hooty868@gmail.com>
2020-10-04 01:10:06 +0800

    new project modify folder path

---
+++ app.js
@@ -0,0 +1,39 @@
+//include package and define server related variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateSentence = require('./generateSentence.js')
+const app = express()
+const port = 3000
+
+// data for Generater
+const task = {
+  engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+  designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+  entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+
+}
+const phrase = ['很簡單', '很容易', '很快', '很正常']
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const sentence = generateSentence(options, task, phrase)
+  res.render('index', { options: options, sentence: sentence })
+})
+
+//starts the express server and listening for connections
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
---
+++ generateSentence.js
@@ -0,0 +1,32 @@
+// define generatePassword function
+function generate_Sentence(options, task, phrase) {
+  // define sentence structure
+  //身為一個 + 'target' + 'task' + 'phrase' 吧！
+  //establish the first words
+  let sentence = ''
+  //detect the mount of user choose
+  if (Object.keys(options).length === 0 || Object.keys(options).length > 1) {
+    sentence += `只需選擇一個角色，你選了${Object.keys(options).length}個`
+  }
+  else {
+    switch (Object.keys(options)[0]) {
+      case 'developer': sentence += ('身為一個工程師' + sample(task.engineer) + sample(phrase) + '吧！');
+        break;
+      case 'designer': sentence += ('身為一個設計師' + sample(task.designer) + sample(phrase) + '吧！');
+        break;
+      case 'founder': sentence += ('身為一個創業家' + sample(task.entrepreneur) + sample(phrase) + '吧！');
+        break;
+    }
+  }
+  // return the generated sentence
+  return sentence
+}
+
+// define sample function to randomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+// export generatePassword function for other files to use
+module.exports = generate_Sentence
